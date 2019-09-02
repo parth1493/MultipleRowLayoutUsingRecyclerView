630686a322809f397baaeb1c0b1f6df384566557
Parth <parth1493@gmail.com>
2019-09-02 01:33:31 -0400

    Navigation drawer example.

--- app/src/main/java/com/example/multiplerowlayoutusingrecyclerview/app/MainActivity.java
+++ app/src/main/java/com/example/multiplerowlayoutusingrecyclerview/app/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.multiplerowlayoutusingrecyclerview;
+package com.example.multiplerowlayoutusingrecyclerview.app;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,25 +8,33 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import com.example.multiplerowlayoutusingrecyclerview.R;
 import com.example.multiplerowlayoutusingrecyclerview.adapter.RecyclerAdapter;
 import com.example.multiplerowlayoutusingrecyclerview.model.Landscape;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpToolbar();
-
+        setUpDrawer();
         setUpRecyclerView();
     }
 
     private void setUpToolbar() {
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Multiple Rows Layout Demo");
         toolbar.inflateMenu(R.menu.menu_main);
+    }
+
+    private void setUpDrawer() {
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.nav_drwr_fragment);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerFragment.setUpDrawer(R.id.nav_drwr_fragment, drawerLayout, toolbar);
     }
 
     private void setUpRecyclerView() {
